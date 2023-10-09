e21312118a5340d112ff56c972561277e4947d8e
Pratyusha Sarkar <110172921+pratyusha-sarkar1729@users.noreply.github.com>
2023-10-10 05:02:36 +0530

    Update and rename code_mse.R to code__mixture_estimator_mse.R

---
+++ code__mixture_estimator_mse.R
@@ -0,0 +1,185 @@
+library(mvtnorm)
+
+# Example:
+set.seed(1)
+N <- 50
+X <- matrix(rnorm(N * 25), ncol = 25)
+true_theta <- (rep(0.2, 25))
+true_sigma <- 2
+y <- rnorm(N, as.vector(X %*% matrix(true_theta, ncol = 1)), true_sigma)
+theta_0 <- as.matrix(rep(0, 25))
+Sigma <- diag(25)
+num_samples <- 500
+initial_theta <- rep(0.1, 25)
+s.cand <- 0.008
+
+
+# Function to calculate the posterior log probability p(theta|y)
+calculate_posterior_log <- function(y, X, true_theta, sigma, theta_0, Sigma) {
+  likelihood <- -dmvnorm(y, mean = X %*% true_theta, sigma = sigma,log = TRUE)
+  prior <- -dmvnorm(true_theta, mean = theta_0, sigma = Sigma, log = TRUE)
+  posterior <- likelihood + prior
+  return(posterior)
+}
+calculate_posterior_log(y,X,true_theta,true_sigma*diag(length(y)),theta_0,Sigma)
+sigma = true_sigma*diag(length(y))
+
+# Function to calculate the inverse likelihood (1 / p(y|theta))
+calculate_inverse_likelihood <- function(y, X, true_theta, sigma) {
+  inverse_likelihood <- 1 / -dmvnorm(y, mean = X %*% true_theta, sigma = sigma,log = TRUE)
+  return(inverse_likelihood)
+}
+calculate_inverse_likelihood(y,X,true_theta,true_sigma*diag(length(y)))
+
+
+# Function to calculate the combined posterior log probability p(theta|y) * sum(1 / p(y|theta))
+calculate_combined_posterior_log <- function(y, X, true_theta, sigma, theta_0, Sigma) {
+  posterior_log <- calculate_posterior_log(y, X, true_theta, sigma, theta_0, Sigma)
+  inverse_likelihood_sum <- sum(calculate_inverse_likelihood(y, X, true_theta, sigma))
+  combined_posterior_log <- posterior_log + (inverse_likelihood_sum)
+  return(combined_posterior_log)
+}
+calculate_combined_posterior_log(y,X,true_theta,true_sigma*diag(length(y)),theta_0,Sigma)
+
+
+# Function to draw samples from the combined posterior using Metropolis-Hastings
+theta_samples_from_combined_posterior <- function(y, X, num_samples, theta_0, Sigma, sigma, s.cand) {
+  num_parameters <- length(theta_0)
+  samples <- matrix(0, nrow = num_samples, ncol = num_parameters)
+  current_theta <- theta_0
+  #s = 1
+  for (s in 1:num_samples) {
+    proposal_theta <- as.vector(rmvnorm(1, mean = current_theta, sigma = s.cand * diag(num_parameters)))
+    
+    # Calculate the Metropolis-Hastings acceptance ratio in log space
+    combined_posterior_current_log <- calculate_combined_posterior_log(y, X, current_theta, sigma, theta_0, Sigma)
+    combined_posterior_proposal_log <- calculate_combined_posterior_log(y, X, proposal_theta, sigma, theta_0, Sigma)
+    
+    log_ratio <- combined_posterior_proposal_log - combined_posterior_current_log
+    
+    if ( !is.na(log_ratio) && log(runif(1)) < log_ratio) {
+      current_theta <- proposal_theta
+    }
+    
+    samples[s, ] <- current_theta
+  }
+  
+  return(samples)
+}
+
+
+theta_samples <- theta_samples_from_combined_posterior(y, X, num_samples, initial_theta, Sigma, true_sigma*diag(length(y)), s.cand)
+theta_samples
+dim(theta_samples)
+length(unique(theta_samples[,1])) / nrow(theta_samples)
+# Function to calculate weights for each data point
+calculate_weights <- function(y, X, true_theta, true_sigma) {
+  inverse_likelihoods <- numeric(0)
+  
+  for(i in 1: length(y)){
+    inverse_likelihoods[i] <- -(1 / dnorm(y[i], mean = X[i,] %*% true_theta, sd = true_sigma,log = TRUE))
+  }
+  weights <- inverse_likelihoods / sum(inverse_likelihoods)
+  return(weights)
+}
+calculate_weights(y,X,true_theta,true_sigma)
+
+
+# Function to calculate the desired quantity (predictive distribution for each data point)
+calculate_mixture_estimator <- function(y, X, theta_samples, sigma) {
+  num_samples <- nrow(theta_samples)
+  num_observations <- length(y)
+  #i= 100
+  vector <- numeric()
+  
+  weighted_likelihoods <- matrix(0,num_samples,num_observations)
+  weights <- matrix(0,num_samples,num_observations)
+  #s =20
+  for (s in 1:num_samples) {
+    weights[s,] <- calculate_weights(y, X, t(theta_samples[s,]), true_sigma)
+    weighted_likelihoods[s,] <- -(dmvnorm((y), mean = X %*% (theta_samples[s, ]), sigma = sigma,log = TRUE)) + log(weights[s,])
+  }
+  
+  # Store the weighted_likelihoods for this observation in the matrix
+  vector <- cbind(weighted_likelihoods,as.matrix(weights))
+  
+  return(vector)
+}
+
+
+weighted_likelihoods_matrix <- calculate_mixture_estimator(y, X, theta_samples, true_sigma*diag(length(y)))
+vector_1 <- weighted_likelihoods_matrix
+dim(vector_1)
+
+vec_1_sum <- numeric()
+for (i in 1:(2*num_observations)) {
+  vec_1_sum[i] <- sum(vector_1[,i])
+}
+
+calculate_vector_2 <- function(x){
+  n <- length(x) / 2  # Assuming 'x' is your input vector of length 2n
+  g_2_matrix <- matrix(0, nrow = n, ncol =  1)  # Initialize the result matrix
+  
+  #i = 2
+  for (i in 1:n) {
+    g_2_matrix[i, ] <- x[i] / (x[i+n])
+  }
+  
+  return(g_2_matrix)
+}
+vector_2 <- calculate_vector_2(vec_1_sum)
+# Calculate the desired quantity (predictive distribution) for each data point
+#mixture_estimator <- calculate_mixture_estimator(y, X, theta_samples, true_sigma)
+
+#mu_i_hat = mixture_estimator
+mix= (log(vector_2))
+
+
+# Compute p(y_i|y_(-i))
+compute_conditional_pdf_Y_i_given_Y_minus_i <- function(Y, X, Sigma_0, mu_0, sigma_0) {
+  n <- length(Y)  # Number of observations
+  pdf_values <- numeric(n)  # Initialize a vector to store PDF values
+  #i = 1
+  log_densities <- matrix(0, nrow = n, ncol = 1)  #
+  for (i in 1:n) {
+    Y_i <- Y[i]
+    X_i <- X[i,i ]
+    # Exclude the i-th observation
+    Y_minus_i <- Y[-i]
+    X_minus_i <- X[-i,-i ]  # Corrected X for X_{-i}
+    Sigma_0_minus_i <- Sigma_0[-i,-i]
+    mu_0_minus_i <- mu_0[-i]
+    # Extract the i-th observation
+   
+    
+    # Calculate Lambda_minus_i
+    Lambda_minus_i <- solve((t(X_minus_i) %*% X_minus_i)/(sigma_0^2) + solve( Sigma_0_minus_i))
+    
+    # Calculate eta_minus_i
+    eta_minus_i <- solve((t(X_minus_i) %*% X_minus_i)/(sigma_0^2) + solve( Sigma_0_minus_i)) %*%
+      (solve(Sigma_0_minus_i) %*% mu_0_minus_i + (1 / (sigma_0^2)) * t(X_minus_i) %*% as.matrix(Y_minus_i))
+    
+    # Calculate mean and variance of the conditional distribution
+    mean_Y_i_given_Y_minus_i <- X_minus_i %*% eta_minus_i
+    variance_Y_i_given_Y_minus_i <- sigma_0^2*diag(nrow(X_minus_i)) + (X_minus_i) %*% Lambda_minus_i %*% t(X_minus_i)
+    
+    # Compute the PDF value for Y_i
+    #pdf_values[i] <- dnorm(Y[i], mean = mean_Y_i_given_Y_minus_i, sd = sqrt(variance_Y_i_given_Y_minus_i),log= TRUE)
+    log_density <- dmvnorm(Y_minus_i, mean = mean_Y_i_given_Y_minus_i, sigma = (variance_Y_i_given_Y_minus_i))
+    log_densities[i] <- log_density
+    
+}
+  
+  return(log_densities)
+}
+Y = y
+Sigma_0 = Sigma
+mu_0 = theta_0
+sigma_0 = true_sigma
+test <- compute_conditional_pdf_Y_i_given_Y_minus_i(y,X,Sigma,theta_0,true_sigma)
+original = (log(test))
+
+# Calculate the Mean Squared Error (MSE)
+mse <- mean((original - mix)^2)
+print(paste("MSE:", mse))
+
